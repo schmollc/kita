3a39474ba40fab11c6a8754168d98cfdb5af1dc7
Christian Schmoll <it1f36@retail-sc.com>
2018-05-15 06:38:16 +0200

    REL-351 create NavigationsConstants.

---
+++ kita-web/src/main/java/com/kita/web/pagebean/NavigationConstants.java
@@ -0,0 +1,20 @@
+package com.kita.web.pagebean;
+
+import java.io.Serializable;
+
+/**
+ * Bloch, Joshua, Effective Java, 2nd Edition, p. 98
+ *
+ * @since 15.05.2018
+ *
+ */
+public final class NavigationConstants implements Serializable {
+	private static final long serialVersionUID = 2482809151474877656L;
+
+	private NavigationConstants() {
+		// restrict instantiation
+	}
+
+	public static final String TOURNAMENT_EVENT_EDIT_DIALOG_ID = "event/tournamentEventEditDialog";
+
+}
