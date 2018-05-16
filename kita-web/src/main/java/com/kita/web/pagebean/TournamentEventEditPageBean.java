e15eb31c444b3d3c587fbf8750b90a0b12404b44
schmollc <c_schmoll@hotmail.com>
2018-05-16 18:34:14 +0200

    REL-351 introduce the calendar from primefaces.

--- kita-web/src/main/java/com/kita/web/pagebean/TournamentEventEditPageBean.java
+++ kita-web/src/main/java/com/kita/web/pagebean/TournamentEventEditPageBean.java
@@ -1,6 +1,7 @@
 package com.kita.web.pagebean;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
@@ -33,6 +34,8 @@ public class TournamentEventEditPageBean implements Serializable {
 
 	TournamentEvent workingTournamentEvent = null;
 
+	private Date date2;
+	
 	public TournamentEventEditPageBean() {
 		tournamentEventBridge = new TournamentEventBridgeImpl();
 	}
@@ -112,5 +115,13 @@ public class TournamentEventEditPageBean implements Serializable {
 
 	private void refreshGui() {
 		workingTournamentEvent = getTournamentEvent(workingTournamentEvent.getUuid());
+	}
+
+	public Date getDate2() {
+		return date2;
+	}
+
+	public void setDate2(Date date2) {
+		this.date2 = date2;
 	}
 }
